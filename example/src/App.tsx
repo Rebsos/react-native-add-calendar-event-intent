6748706cc355e45325de60874b036e0039e065a9
Rebs <rebs85@gmail.com>
2024-10-20 23:42:25 +0200

    feat: add demo button

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,6 +1,9 @@
 import { useState, useEffect } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-add-calendar-event-intent';
+import { StyleSheet, View, Text, Pressable } from 'react-native';
+import {
+  multiply,
+  addCalendarEventIntent,
+} from 'react-native-add-calendar-event-intent';
 
 export default function App() {
   const [result, setResult] = useState<number | undefined>();
@@ -12,6 +15,22 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Result: {result}</Text>
+      <Pressable
+        onPress={async () => {
+          await addCalendarEventIntent({
+            title: 'My title',
+            description: 'My description',
+            location: 'Berlin',
+            startAt: new Date().getTime(),
+            endAt: new Date().getTime() + 1000 * 60 * 120,
+            // allDay: true,
+            // recurrenceRule: 'FREQ=DAILY;COUNT=3',
+            // id: 123,
+          });
+        }}
+      >
+        <Text>PRESS</Text>
+      </Pressable>
     </View>
   );
 }
